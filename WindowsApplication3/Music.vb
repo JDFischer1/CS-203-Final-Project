c3f0e55b2eb42a12b6820df9a2e26c8e49f46c9d
DESKTOP-N6MLFGH\James Fischer <fiscjd01@students.ipfw.edu>
2016-10-29 16:27:59 -0400

    Added abstract class and laid out additional forms

---
+++ WindowsApplication3/Music.vb
@@ -0,0 +1,10 @@
+﻿Public MustInherit Class Music
+    Private mTitle As String
+    Private mArtist As String
+
+    Public Sub New(ByVal pTitle As String, ByVal pArtist As String)
+        mTitle = pTitle
+        mArtist = pArtist
+    End Sub
+
+End Class
---
+++ WindowsApplication3/Playlist.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Playlist
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.radPop = New System.Windows.Forms.RadioButton()
+        Me.radRock = New System.Windows.Forms.RadioButton()
+        Me.radCountry = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(372, 67)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(296, 224)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(487, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Playlist"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton4)
+        Me.GroupBox1.Controls.Add(Me.radCountry)
+        Me.GroupBox1.Controls.Add(Me.radRock)
+        Me.GroupBox1.Controls.Add(Me.radPop)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 67)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(222, 224)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Genre"
+        '
+        'radPop
+        '
+        Me.radPop.AutoSize = True
+        Me.radPop.Location = New System.Drawing.Point(19, 36)
+        Me.radPop.Name = "radPop"
+        Me.radPop.Size = New System.Drawing.Size(62, 24)
+        Me.radPop.TabIndex = 0
+        Me.radPop.TabStop = True
+        Me.radPop.Text = "Pop"
+        Me.radPop.UseVisualStyleBackColor = True
+        '
+        'radRock
+        '
+        Me.radRock.AutoSize = True
+        Me.radRock.Location = New System.Drawing.Point(19, 78)
+        Me.radRock.Name = "radRock"
+        Me.radRock.Size = New System.Drawing.Size(71, 24)
+        Me.radRock.TabIndex = 1
+        Me.radRock.TabStop = True
+        Me.radRock.Text = "Rock"
+        Me.radRock.UseVisualStyleBackColor = True
+        '
+        'radCountry
+        '
+        Me.radCountry.AutoSize = True
+        Me.radCountry.Location = New System.Drawing.Point(19, 122)
+        Me.radCountry.Name = "radCountry"
+        Me.radCountry.Size = New System.Drawing.Size(89, 24)
+        Me.radCountry.TabIndex = 2
+        Me.radCountry.TabStop = True
+        Me.radCountry.Text = "Country"
+        Me.radCountry.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(19, 163)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(89, 24)
+        Me.RadioButton4.TabIndex = 3
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Custom"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(372, 323)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(86, 45)
+        Me.btnOK.TabIndex = 5
+        Me.btnOK.Text = "OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(582, 323)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(86, 45)
+        Me.btnCancel.TabIndex = 6
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'Playlist
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(809, 416)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Playlist"
+        Me.Text = "Playlist"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents radCountry As RadioButton
+    Friend WithEvents radRock As RadioButton
+    Friend WithEvents radPop As RadioButton
+    Friend WithEvents btnOK As Button
+    Friend WithEvents btnCancel As Button
+End Class
