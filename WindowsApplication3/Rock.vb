1c583eefd8afb931fec19480683d4604d947b780
DESKTOP-N6MLFGH\James Fischer <fiscjd01@students.ipfw.edu>
2016-10-31 18:01:38 -0400

    Create child classes

---
+++ WindowsApplication3/Rock.vb
@@ -0,0 +1,10 @@
+﻿Public Class Rock
+    Inherits Music
+
+    Private Property Genre As String
+
+    Public Sub New(ByVal pTitle As String, ByVal pArtist As String, ByVal pGenre As String)
+        MyBase.New(pTitle, pArtist)
+        Genre = pGenre
+    End Sub
+End Class
